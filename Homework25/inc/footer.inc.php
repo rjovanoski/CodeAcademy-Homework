f3f43da691353eb0e0d711dc8a777e83227496ce
Radoslav <61637724+rjovanoski@users.noreply.github.com>
2020-05-20 18:04:35 +0200

    Update footer.inc.php

--- Homework25/inc/footer.inc.php
+++ Homework25/inc/footer.inc.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <div class="container-fluid bg-dark text-white pb-4">
         <div class="row p-4">
 
@@ -20,27 +19,4 @@
 
             The images are taken from <a href="https://unsplash.com">Unsplash.com</a> to avoid copyright issues.
         </div>
-=======
-<div class="container-fluid bg-dark text-white pb-4">
-        <div class="row p-4">
-
-            <div class="col  text-center">
-                Follow us on
-                <ul class="list-unstyled list-inline social-icons mt-1">
-                    <li class="list-inline-item"><a href="#"><i class="fab fa-2x fa-facebook-square"></i></a> </li>
-                    <li class="list-inline-item"><a href="#"><i class="fab fa-2x fa-google"></i></a> </li>
-                    <li class="list-inline-item"><a href="#"><i class="fab fa-2x fa-twitter-square"></i></a> </li>
-                    <li class="list-inline-item"><a href="#"><i class="fab fa-2x fa-instagram"></i></a> </li>
-                    <li class="list-inline-item"><a href="#"><i class="fab fa-medium fa-2x"></i></a></li>
-                </ul>
-            </div>
-
-        </div>
-        <div class="text-center">
-            <span class="fas fa-code text-primary"></span>&nbsp;&nbsp;with&nbsp;&nbsp;<span class="fas fa-heart"
-                style="color: red"></span> By Developers<br>
-
-            The images are taken from <a href="https://unsplash.com">Unsplash.com</a> to avoid copyright issues.
-        </div>
->>>>>>> 1702b7b0f7ff6e56a4dbecefa98b1b4f3383e842
 </div>
