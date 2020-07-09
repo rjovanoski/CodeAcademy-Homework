5c07f28fbdab54876824017b5d262c0539e3840d
Radoslav <61637724+rjovanoski@users.noreply.github.com>
2020-07-09 03:28:03 +0200

    Update app.blade.php

--- Homework47/resources/views/layouts/app.blade.php
+++ Homework47/resources/views/layouts/app.blade.php
@@ -36,8 +36,11 @@
     <div id="app">
         
         @include('includes.nav')
+        
         <main class="py-4">
+        
             @yield('content')
+        
         </main>
     </div>
 </body>
