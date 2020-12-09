910f660d6af335c6b930feefd3af922097db03fa
rjovanoski <rade_j@hotmail.com>
2020-12-09 09:54:11 +0100

    Final Project

--- CA-Final/public/js/app.js
+++ CA-Final/public/js/app.js
@@ -12886,8 +12886,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /var/www/html/final/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /var/www/html/final/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! D:\Final Project\CodeAcademy-Homework\CA-Final\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! D:\Final Project\CodeAcademy-Homework\CA-Final\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
