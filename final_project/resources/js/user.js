26de0571ae7ff451caf1d4e829a49d95e585ff20
rjovanoski <rade_j@hotmail.com>
2021-01-12 00:05:31 +0100

    Updated

--- final_project/resources/js/user.js
+++
@@ -1,8 +0,0 @@
-let toggle = document.querySelector('.menu');
-let dropdown = document.querySelector('.dropdown-menu');
-let main = document.querySelector('.profile');
-
-toggle.addEventListener("click", () =>{
-dropdown.classList.toggle("open");
-main.classList.toggle("down"); 
-});
--- final_project/resources/views/components/alert.blade.php
+++
@@ -1,7 +0,0 @@
-<div>
-    @if(session()->has('message'))
-        <div class="alert alert-success">{{ session()->get('message') }}</div>
-    @elseif(session()->has('error'))
-        <div class="alert alert-danger">{{ session()->get('error') }}</div>
-    @endif
-</div>
